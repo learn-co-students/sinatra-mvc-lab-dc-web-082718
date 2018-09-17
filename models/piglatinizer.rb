8d6601e16f10193b0838e8b54a3a4b2b50e79b11
jb5595 <jacobberman1995@gmail.com>
2018-09-17 18:01:17 -0400

    refactored

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,20 +1,26 @@
 class PigLatinizer
 
-  def piglatinize(input, first_run = true)
+  def piglatinize(input)
     if input.include?(" ")
       @words = input.split(" ").collect do |word|
-        piglatinize(word)
+        piglatinize_word(word)
       end
-      return @words.join(" ")
+    return @words.join(" ")
+    else
+      piglatinize_word(input)
+    end
+  end
 
-    elsif /[AEIOU]/i.match(input[0]) && first_run
+  def piglatinize_word(input, first_run = true)
+    if /[AEIOU]/i.match(input[0]) && first_run
       @piglatin = input + "w"
     elsif /[^AEIOU]/i.match(input[0])
       first_run = false
       @piglatin = input[1..input.length] + input[0]
-      piglatinize(@piglatin, first_run)
+      piglatinize_word(@piglatin, first_run)
     end
     @piglatin + "ay"
   end
+
 
 end
